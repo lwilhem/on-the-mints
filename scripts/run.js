cae9b8cb215225dd5ab9076c167e9efcc525d9bc
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-05-02 00:52:09 +0200

    :sparkles: Shiny Nft For The Barkeeper => might have to fix text-repeat

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,7 @@
 const main = async () => {
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   // We pass in "ninja" to the constructor when deploying
-  const domainContract = await domainContractFactory.deploy(".DrinksOnMe");
+  const domainContract = await domainContractFactory.deploy("TheTab");
   await domainContract.deployed();
 
   console.log("Contract deployed to:", domainContract.address);
