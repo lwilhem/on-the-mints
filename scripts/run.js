2fd01de6e3928c4bb36ff31ffaa277092c27cf8f
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-05-01 23:48:22 +0200

    :construction: let's make some Nft's

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,20 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
-    await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
-    
-    const txn = await domainContract.register("doom");
-    await txn.wait();
-  
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  // We pass in "ninja" to the constructor when deploying
+  const domainContract = await domainContractFactory.deploy(".DrinksOnMe");
+  await domainContract.deployed();
+
+  console.log("Contract deployed to:", domainContract.address);
+
+  // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+  let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+  await txn.wait();
+
+  const address = await domainContract.getAddress("mortal");
+  console.log("Owner of domain mortal:", address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
   
 const runMain = async () => {
